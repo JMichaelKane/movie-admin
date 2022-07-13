2f6fd55caec50fe821a3e2201cf0255ab1fbec99
jinzhongjia <jinzhongjia@qq.com>
2022-07-13 22:11:45 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
 <template>
 	<n-scrollbar style="max-height: 100vh">
 		<n-notification-provider>
-		<!-- 通知 -->
+			<!-- 通知 -->
 			<n-dialog-provider>
-			<!-- 对话框 -->
+				<!-- 对话框 -->
 				<n-message-provider>
-				<!-- 信息 -->
+					<!-- 信息 -->
 					<router-view />
 				</n-message-provider>
 			</n-dialog-provider>
@@ -16,30 +16,30 @@
 </template>
 
 <style>
-	body {
-		margin: 0;
-	}
-	.user-drag {
-		-webkit-user-drag: none;
-	}
-	a {
-		text-decoration: none; /* 去除默认的下划线 */
-		outline: none; /* 去除旧版浏览器的点击后的外虚线框 */
-		color: #000; /* 去除默认的颜色和点击后变化的颜色 */
-	}
-	.relative {
-		position: relative;
-	}
-	.absolute {
-		position: absolute;
-	}
-	.flex {
-		display: flex;
-	}
-	.justify-between {
-		justify-content: space-between;
-	}
-	.border-box {
-		box-sizing: border-box;
-	}
+body {
+	margin: 0;
+}
+.user-drag {
+	-webkit-user-drag: none;
+}
+a {
+	text-decoration: none; /* 去除默认的下划线 */
+	outline: none; /* 去除旧版浏览器的点击后的外虚线框 */
+	color: #000; /* 去除默认的颜色和点击后变化的颜色 */
+}
+.relative {
+	position: relative;
+}
+.absolute {
+	position: absolute;
+}
+.flex {
+	display: flex;
+}
+.justify-between {
+	justify-content: space-between;
+}
+.border-box {
+	box-sizing: border-box;
+}
 </style>
